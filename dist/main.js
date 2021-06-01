d20dd6939ee37248f83e92ff7006872bc5239e2a
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-01 06:09:26 -0300

    Feat: Input event on textarea and adding your content to preview

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('Hello');
+var textarea = document.getElementById('editor');
+var preview = document.getElementById('preview');
+function handleTextareaInput(event) {
+    var target = event.target;
+    if (preview) {
+        preview.innerHTML = target.value;
+    }
+}
+textarea === null || textarea === void 0 ? void 0 : textarea.addEventListener('input', handleTextareaInput);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('Hello')
+const textarea: HTMLElement | null = document.getElementById('editor');
+const preview: HTMLElement | null = document.getElementById('preview');
+
+function handleTextareaInput(this: HTMLElement, event: Event): void {
+  const target = event.target as HTMLTextAreaElement;
+
+  if (preview) {
+    preview.innerHTML = target.value;
+  }
+}
+
+textarea?.addEventListener('input', handleTextareaInput);
