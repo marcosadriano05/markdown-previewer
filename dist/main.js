6f9c94bcc2e215dfcfd302a478d7596fb56cd5dd
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-01 06:52:04 -0300

    Feat: function to format line-breaks in textarea

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,15 @@
 "use strict";
 var textarea = document.getElementById('editor');
 var preview = document.getElementById('preview');
+function formatText(text) {
+    var separatedTextByEndLines = text.split('\n');
+    return separatedTextByEndLines.reduce(function (acc, cur) { return acc + '<br />' + cur; });
+}
 function handleTextareaInput(event) {
     var target = event.target;
+    var formatedText = formatText(target.value);
     if (preview) {
-        preview.innerHTML = target.value;
+        preview.innerHTML = formatedText;
     }
 }
 textarea === null || textarea === void 0 ? void 0 : textarea.addEventListener('input', handleTextareaInput);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 const textarea: HTMLElement | null = document.getElementById('editor');
 const preview: HTMLElement | null = document.getElementById('preview');
 
+function formatText(text: string): string {
+  const separatedTextByEndLines = text.split('\n');
+
+  return separatedTextByEndLines.reduce((acc, cur) => acc + '<br />' + cur);
+}
+
 function handleTextareaInput(this: HTMLElement, event: Event): void {
   const target = event.target as HTMLTextAreaElement;
+  const formatedText = formatText(target.value);
 
   if (preview) {
-    preview.innerHTML = target.value;
+    preview.innerHTML = formatedText;
   }
 }
 
