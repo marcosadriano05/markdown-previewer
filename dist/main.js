6bc8d655d8652de48b69d9c35eaaf6ac4bc56082
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-04 20:20:10 -0300

    Adding marked to convert markdown text to html

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,31 @@
 "use strict";
-var textarea = document.getElementById('editor');
-var preview = document.getElementById('preview');
+const preview = document.getElementById('preview');
+const textarea = document.getElementById('editor');
+const previewText = '# Title 1\n## Title 2\n### Title 3\n#### Title 4\n' +
+    '##### Title 5\n###### Title 6\n' +
+    '[Link to Google](https://google.com)\n' +
+    '\n`git push origin main`\n' +
+    '```js\nconst main = "Hello World!"\nconsole.log(main)\n```\n' +
+    '- list 1\n- list 2\n- list 3\n' +
+    '> blockquote\n' +
+    '\n![marcosadriano05](https://github.com/marcosadriano05.png)\n' +
+    '\n**Bolded text**\n';
+if (textarea) {
+    const textareaValue = textarea;
+    textareaValue.value = previewText;
+}
+if (preview) {
+    preview.innerHTML = marked(previewText);
+}
 function formatText(text) {
-    var separatedTextByEndLines = text.split('\n');
-    return separatedTextByEndLines.reduce(function (acc, cur) { return acc + '<br />' + cur; });
+    const separatedTextByEndLines = text.split('\n');
+    return separatedTextByEndLines.reduce((acc, cur) => acc + '\n' + cur);
 }
 function handleTextareaInput(event) {
-    var target = event.target;
-    var formatedText = formatText(target.value);
+    const target = event.target;
+    const formatedText = formatText(target.value);
     if (preview) {
-        preview.innerHTML = formatedText;
+        preview.innerHTML = marked(formatedText);
     }
 }
 textarea === null || textarea === void 0 ? void 0 : textarea.addEventListener('input', handleTextareaInput);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-const textarea: HTMLElement | null = document.getElementById('editor');
 const preview: HTMLElement | null = document.getElementById('preview');
+const textarea: HTMLElement | null = document.getElementById('editor');
+
+const previewText: string = '# Title 1\n## Title 2\n### Title 3\n#### Title 4\n' +
+  '##### Title 5\n###### Title 6\n' +
+  '[Link to Google](https://google.com)\n' +
+  '\n`git push origin main`\n' +
+  '```js\nconst main = "Hello World!"\nconsole.log(main)\n```\n' +
+  '- list 1\n- list 2\n- list 3\n' +
+  '> blockquote\n' +
+  '\n![marcosadriano05](https://github.com/marcosadriano05.png)\n' +
+  '\n**Bolded text**\n'
+
+if (textarea) {
+  const textareaValue = textarea as HTMLTextAreaElement;
+  textareaValue.value = previewText;
+}
+
+if (preview) {
+  preview.innerHTML = marked(previewText)
+}
 
 function formatText(text: string): string {
   const separatedTextByEndLines = text.split('\n');
 
-  return separatedTextByEndLines.reduce((acc, cur) => acc + '<br />' + cur);
+  return separatedTextByEndLines.reduce((acc, cur) => acc + '\n' + cur);
 }
 
 function handleTextareaInput(this: HTMLElement, event: Event): void {
@@ -12,7 +31,7 @@ function handleTextareaInput(this: HTMLElement, event: Event): void {
   const formatedText = formatText(target.value);
 
   if (preview) {
-    preview.innerHTML = formatedText;
+    preview.innerHTML = marked(formatedText);
   }
 }
 
